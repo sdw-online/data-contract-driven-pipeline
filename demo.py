fad3e71f60362ce376010565cc58c78e76c68530
Stephen <stephenoilesanmi@gmail.com>
2024-12-26 17:15:58 +0000

    Create toggle for displaying env variables

--- demo.py
+++ demo.py
@@ -9,6 +9,7 @@ from boto3.exceptions import Boto3Error
 
 
 # Load environment variables
+load_dotenv()
 
 AWS_ACCESS_TOKEN = os.getenv("AWS_ACCESS_TOKEN")
 AWS_SECRET_KEY = os.getenv("AWS_SECRET_KEY")
@@ -16,7 +17,6 @@ AWS_REGION = os.getenv("AWS_REGION")
 BRONZE_BUCKET = os.getenv("BRONZE_BUCKET")
 SILVER_BUCKET = os.getenv("SILVER_BUCKET")
 GOLD_BUCKET = os.getenv("GOLD_BUCKET")
-BUCKET_NAME = os.getenv("BUCKET_NAME")
 
 POSTGRES_CONFIG = {
     "host": os.getenv("POSTGRES_HOST"),
@@ -25,3 +25,19 @@ POSTGRES_CONFIG = {
     "user": os.getenv("POSTGRES_USER"),
     "password": os.getenv("POSTGRES_PASSWORD")
 }
+
+
+
+CHECK_ENV_VARIABLES = True
+
+if CHECK_ENV_VARIABLES:
+    print("Showing env variables:\n")
+    print(f"AWS Access Token:   {AWS_ACCESS_TOKEN}")
+    print(f"AWS Secret Key:     {AWS_SECRET_KEY}")
+    print(f"AWS Region:         {AWS_REGION}")
+    print(f"Bronze Bucket:      {BRONZE_BUCKET}")
+    print(f"Silver bucket:      {SILVER_BUCKET}")
+    print(f"Gold Bucket:        {GOLD_BUCKET}")
+    print(f"Postgres Config:    {POSTGRES_CONFIG}")
+else:
+    print("Setting up environment...")
