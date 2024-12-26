c6e8352caed629db6aeef1ab3495e1c10f108b57
Stephen <stephenoilesanmi@gmail.com>
2024-12-26 17:45:03 +0000

    Validated AWS + Postgres credentials

--- demo.py
+++ demo.py
@@ -4,14 +4,25 @@ from dotenv import load_dotenv
 import pandas as pd
 import json
 import boto3
-from boto3.exceptions import Boto3Error
+from botocore.exceptions import ClientError
 
 
 
-# Load environment variables
+
+"""
+SETUP
+
+1. Load env variables
+2. Validate environment variables (AWS + Postgres)
+3. Initialize S3 client 
+
+"""
+
+
+# 1.1. Load environment variables
 load_dotenv()
 
-AWS_ACCESS_TOKEN = os.getenv("AWS_ACCESS_TOKEN")
+AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY")
 AWS_SECRET_KEY = os.getenv("AWS_SECRET_KEY")
 AWS_REGION = os.getenv("AWS_REGION")
 BRONZE_BUCKET = os.getenv("BRONZE_BUCKET")
@@ -27,12 +38,15 @@ POSTGRES_CONFIG = {
 }
 
 
+# -- 1.2. Validate environment variables
 
-CHECK_ENV_VARIABLES = True
 
-if CHECK_ENV_VARIABLES:
+# Display the environment variables
+SHOW_ENV_VARIABLES = True
+
+if SHOW_ENV_VARIABLES:
     print("Showing env variables:\n")
-    print(f"AWS Access Token:   {AWS_ACCESS_TOKEN}")
+    print(f"AWS Access key:     {AWS_ACCESS_KEY}")
     print(f"AWS Secret Key:     {AWS_SECRET_KEY}")
     print(f"AWS Region:         {AWS_REGION}")
     print(f"Bronze Bucket:      {BRONZE_BUCKET}")
@@ -41,3 +55,33 @@ if CHECK_ENV_VARIABLES:
     print(f"Postgres Config:    {POSTGRES_CONFIG}")
 else:
     print("Setting up environment...")
+
+
+
+# Validate AWS credentials for S3 setup
+if not AWS_ACCESS_KEY or not AWS_SECRET_KEY or not AWS_REGION:
+    raise ValueError(f"[ERROR] - Missing at least one AWS credential in .env file...")
+
+
+
+# Check if all variables for Postgres connection are present
+for key, value in POSTGRES_CONFIG.items():
+    if not value:
+        raise ValueError(f"[ERROR] - Missing at least one Postgres credential for key: {key}")
+
+
+
+
+# 1.3. Initialize S3 client 
+
+try:
+    s3_client = boto3.client(
+        "s3",
+        aws_access_key_id=AWS_ACCESS_KEY,
+        aws_secret_access_key=AWS_SECRET_KEY,
+        region_name=AWS_REGION
+    )
+    print("[INFO] - S3 client initialized successfully")
+except ClientError as e:
+    print(f"[ERROR] - Failed to initialize S3 client: {e}")
+    raise
