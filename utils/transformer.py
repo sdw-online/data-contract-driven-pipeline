b8f0bcf657e2bcaaee791dffa64ed5fba078d861
Stephen <stephenoilesanmi@gmail.com>
2024-12-30 17:19:42 +0000

    Convert len to integer data type

--- utils/transformer.py
+++ utils/transformer.py
@@ -5,11 +5,14 @@ def transform_data(df):
     required_columns    = ["document", "name", "email", "phone", "len"]
     df                  = df[required_columns]
 
-    # Convert columns to string    
+    # Convert columns to string data type    
     df["document"]      = df["document"].astype(str)
     df["name"]          = df["name"].astype(str)
     df["email"]         = df["email"].astype(str)
     df["phone"]         = df["phone"].astype(str)
+
+    # Convert len to integer data type
+    df["len"]           = df["len"].astype(int)
 
     # Remove whitespace from name column
     df["name"]          = df["name"].str.strip()
